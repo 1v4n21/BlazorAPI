5a5fef0d63c7ed6544cd7f0dde053800440608da
Ivan <ivanmartinezzs29@gmail.com>
2024-05-02 12:17:35 +0200

    Comments

--- BlazorCrud.Server/Controllers/DepartamentoController.cs
+++ BlazorCrud.Server/Controllers/DepartamentoController.cs
@@ -13,39 +13,55 @@ namespace BlazorCrud.Server.Controllers
 	{
 		private readonly DbcrudBlazorContext _dbContext;
 
+		//Metodo para obtener la base de datos mediante la cadena de esta
         public DepartamentoController(DbcrudBlazorContext dbContext)
         {
+			//Obtenemos la base de datos
 			_dbContext = dbContext;
         }
 
+		//Permite peticiones get por http a traves de la ruta /Lista
+		//Obtener una lista de todos los departamentos
 		[HttpGet]
 		[Route("Lista")]
 		public async Task<IActionResult> Lista()
 		{
+			//Respuesta de nuestra API
 			var responseAPI = new ResponseAPI<List<DepartamentoDTO>>();
+
+			//Lista de los departamentos
 			var listaDepartamentoDTO = new List<DepartamentoDTO>();
 
 			try
 			{
+				//Bucle para obtener todos los departamentos y guardarlos
 				foreach(var item in await _dbContext.Departamentos.ToListAsync()) 
 				{
 					listaDepartamentoDTO.Add(new DepartamentoDTO
 					{
+						//Obtenemos todos los atributos del objeto departamento
 						IdDepartamento = item.IdDepartamento,
 						Nombre = item.Nombre,
 					});
 
 				}
 
+				//Si no da error = true
 				responseAPI.EsCorrecto = true;
+
+				//Guardamos la lista de departamentos
 				responseAPI.Valor = listaDepartamentoDTO;
 			}
 			catch (Exception ex)
 			{
+				//Si da error = false
 				responseAPI.EsCorrecto = false;
+
+				//Guardamos elmensaje de error
 				responseAPI.Mensaje = ex.Message;
 			}
 
+			//Returnamos la respuesta de la API y Ok
 			return Ok(responseAPI);
 		}
     }
--- BlazorCrud.Server/Controllers/EmpleadoController.cs
+++ BlazorCrud.Server/Controllers/EmpleadoController.cs
@@ -12,24 +12,33 @@ namespace BlazorCrud.Server.Controllers
 	{
 		private readonly DbcrudBlazorContext _dbContext;
 
+		//Constructor para obtener la base de datos utilizada con su cadena de conexion
 		public EmpleadoController(DbcrudBlazorContext dbContext)
 		{
+			//Obtenemos la base de datos
 			_dbContext = dbContext;
 		}
 
+		//Acepta peticiones get http y la ruta es /Lista para acceder
+		//Obtener una lisa de todos los empleados
 		[HttpGet]
 		[Route("Lista")]
 		public async Task<IActionResult> Lista()
 		{
+			//Respuesta al acceder a la ruta
 			var responseAPI = new ResponseAPI<List<EmpleadoDTO>>();
+
+			//Lista de empleados que se va a obtener
 			var listaEmpleadoDTO = new List<EmpleadoDTO>();
 
 			try
 			{
+				//Vamos guardando los empleados de uno en uno en la lista empleados
 				foreach (var item in await _dbContext.Empleados.Include(d => d.IdDepartamentoNavigation).ToListAsync())
 				{
 					listaEmpleadoDTO.Add(new EmpleadoDTO
 					{
+						//Insertamos todos los atributos del objeto empleado
 						IdEmpleado = item.IdEmpleado,
 						NombreCompleto = item.NombreCompleto,
 						IdDepartamento = item.IdDepartamento,
@@ -37,6 +46,7 @@ namespace BlazorCrud.Server.Controllers
 						FechaContrato = item.FechaContrato,
 						Departamento = new DepartamentoDTO
 						{
+							//Obtenemos el departamento del empleado
 							IdDepartamento = item.IdDepartamentoNavigation.IdDepartamento,
 							Nombre = item.IdDepartamentoNavigation.Nombre,
 						}
@@ -44,15 +54,22 @@ namespace BlazorCrud.Server.Controllers
 
 				}
 
+				//Si no da error es igual a true
 				responseAPI.EsCorrecto = true;
+
+				//Guardamos el valor de respuesta de la API
 				responseAPI.Valor = listaEmpleadoDTO;
 			}
 			catch (Exception ex)
 			{
+				//Si da error es igual a false
 				responseAPI.EsCorrecto = false;
+
+				//Guardamos el mensaje de error de la excepcion
 				responseAPI.Mensaje = ex.Message;
 			}
 
+			//Returnamos la respuesta de la API con un OK
 			return Ok(responseAPI);
 		}
 
