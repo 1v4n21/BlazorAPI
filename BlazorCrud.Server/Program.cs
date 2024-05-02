a34cbb6d70d921dd9ee4ee3f0e567f8d56e5b018
Ivan <ivanmartinezzs29@gmail.com>
2024-05-02 09:35:31 +0200

    Construyendo API

--- BlazorCrud.Server/Program.cs
+++ BlazorCrud.Server/Program.cs
@@ -1,3 +1,6 @@
+using BlazorCrud.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbcrudBlazorContext>(opciones =>
+{
+	opciones.UseSqlServer(builder.Configuration.GetConnectionString("cadenaSQL"));
+}
+
+);
 
 var app = builder.Build();
 
