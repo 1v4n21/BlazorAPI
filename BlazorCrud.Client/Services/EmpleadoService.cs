3019297a805aa3b07da74cabb86915175a77f7f9
Ivan <ivanmartinezzs29@gmail.com>
2024-05-03 12:20:35 +0200

    empleado Service

---
+++ BlazorCrud.Client/Services/EmpleadoService.cs
@@ -0,0 +1,40 @@
+﻿using BlazorCrud.Share;
+using System.Net.Http.Json;
+
+namespace BlazorCrud.Client.Services
+{
+    public class EmpleadoService : IEmpleadoService
+    {
+        private readonly HttpClient _http;
+
+        public EmpleadoService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public Task<List<EmpleadoDTO>> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EmpleadoDTO> Buscar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Editar(EmpleadoDTO empleado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Guardar(EmpleadoDTO empleado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
